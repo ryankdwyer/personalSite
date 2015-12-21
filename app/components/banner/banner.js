3619388b2e1823e69a4a245239b703ac60acae1f
Ryan Dwyer <ryankdwyer@gmail.com>
2015-12-21 10:23:26 -0500

    photo only shows up on main page

--- app/components/banner/banner.js
+++ app/components/banner/banner.js
@@ -1,6 +1,12 @@
 app.directive('banner', function () {
     return {
         restrict: 'E',
-        templateUrl: 'components/banner/banner.html'
+        templateUrl: 'components/banner/banner.html',
+        controller: function ($scope, $state, $location, $rootScope) {
+            $scope.currState = 'main';
+            $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState) {
+                $scope.currState = toState.name;
+            })
+        }
     }
 });
--- app/shared/header/header.controller.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by ryan on 12/18/15.
- */
