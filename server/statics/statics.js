1b337294cbe6fa6d0c80d6e0e9d2385ae65887e3
Ryan Dwyer <ryan@Ryans-MacBook-Pro.local>
2016-03-08 08:52:49 -0500

    fixing dependencies

--- server/statics/statics.js
+++ server/statics/statics.js
@@ -1,16 +1,13 @@
 'use strict';
 
 const express = require('express'),
-    router = express.Router(),
-    path = require('path');
+  router = express.Router(),
+  path = require('path');
 
 const rootPath = path.join(__dirname, '../..'),
-    nodeModules = path.join(rootPath, 'node_modules'),
-    appPath = path.join(rootPath, 'app'),
-    assetspath = path.join(rootPath, 'assets');
-
-console.log('rootPath', rootPath);
-console.log('nodeModules', nodeModules);
+  nodeModules = path.join(rootPath, 'node_modules'),
+  appPath = path.join(rootPath, 'app'),
+  assetspath = path.join(rootPath, 'assets');
 
 router.use(express.static(nodeModules));
 router.use(express.static(appPath));
